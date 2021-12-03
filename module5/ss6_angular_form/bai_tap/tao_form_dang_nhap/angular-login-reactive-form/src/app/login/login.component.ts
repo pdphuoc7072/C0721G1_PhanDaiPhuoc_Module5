5f55144e88d3e9efef326d1c7dcdbd24d6ebbbfb
Phan Đại Phước <pdphuoc7072@gmail.com>
2021-12-03 17:18:40 +0700

    update

---
+++ module5/ss6_angular_form/bai_tap/tao_form_dang_nhap/angular-login-reactive-form/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    // tslint:disable-next-line:max-line-length
+    email: new FormControl('', [Validators.required, Validators.pattern('^(?:^|\\s)[\\w!#$%&\'*+/=?^`{|}~-](\\.?[\\w!#$%&\'*+/=?^`{|}~-]+)*@\\w+[.-]?\\w*\\.[a-zA-Z]{2,3}\\b$')]),
+    // tslint:disable-next-line:max-line-length
+    password: new FormControl('', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&_])[A-Za-z\d$@$!%*?&_].{5,19}')])
+  });
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit(loginForm: FormGroup) {
+    console.log(loginForm.value);
+  }
+}
